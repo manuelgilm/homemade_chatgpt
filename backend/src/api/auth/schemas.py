a9e3633e38d90bf519e433e2ddad203c53bb028d
Manuel Gil <manuelgilsitio@gmail.com>
2024-11-24 11:58:57 -0500

    updating schemas

--- backend/src/api/auth/schemas.py
+++ backend/src/api/auth/schemas.py
@@ -1,36 +1,42 @@
 from pydantic import BaseModel
-from pydantic import Field
+from pydantic import Field 
 import uuid
-from datetime import datetime
-
+from datetime import datetime 
 
 class UserCreateModel(BaseModel):
     first_name: str = Field(max_length=25)
     last_name: str = Field(max_length=25)
     username: str = Field(max_length=8)
     email: str = Field(max_length=50)
-    password: str = Field(min_length=6)
+    password:str = Field(min_length=6)
 
     model_config = {
-        "json_schema_extra": {
-            "example": {
-                "first_name": "John",
-                "last_name": "Doe",
-                "username": "johndoe",
-                "email": "johndoe123@co.com",
-                "password": "password123",
-            }
+        'json_schema_extra': {
+            'example': {
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'username': 'johndoe',
+                'email': 'johndoe123@co.com',
+                'password': 'password123'
         }
     }
-
+    }
 
 class UserModel(BaseModel):
     uid: uuid.UUID
-    username: str
-    email: str
-    first_name: str
-    last_name: str
+    username:str 
+    email:str 
+    first_name: str 
+    last_name: str 
     is_verified: bool
     password_hash: str = Field(exclude=True)
-    created_at: datetime
-    updated_at: datetime
+    created_at:datetime
+    updated_at:datetime 
+
+class UserLoginModel(BaseModel):
+    username: str
+    password: str
+
+    
+
+
